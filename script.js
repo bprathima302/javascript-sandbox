b9bbc407dbaaa63343b7b149dee7bb7f2c02ae63
bprathima302 <b.prathima302@gmail.com>
2023-10-25 15:29:50 -0400

    InsertionSort

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-console.log(100);
- const x = 100;
- console.log(x);
 
- console.error('Alert error');
- console.warn('Alert error');
- console.table({name :'Brad', email: 'b.prathima302@email.com'});
- 
- console.group('simple');
-    console.log(x);
-    console.error('Alert error');
-    console.warn('Alert error');
-console.groupEnd();
+function insertionSort2(n, arr) {
+    // Write your code here
+    for(let i=1; i<=arr.length-1; i++){
+        let k= i;
+        for(j=i-1; j>=0; j--){
+            if(arr[k] < arr[j]){
+                let temp = arr[k];
+                arr[k]= arr[j];
+                arr[j]= temp;
+                k--;
+            }
+        }
+        console.log(...arr);
+    }
+}
+
+insertionSort2(5, [7, 8, 6, 5, 4, 3, 2, 1])
